26b586510d4bba0f4601db4a7fc402c05c9a376c
Davonne <davonnejv@gmail.com>
2025-11-25 22:53:00 -0600

    feature: hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,28 @@
 export default function Hero() {
   return (
     <section className="relative bg-[url('/images/skate.jpg')] bg-cover bg-center min-h-screen">
-      <div className="absolute inset-0 bg-black/50"></div>
+      <div className="absolute inset-0 bg-black/70"></div>
       <div className="relative z-10 flex  items-center justify-center h-full">
-        <div className="flex flex-col gap-4 text-center text-6xl font-zen mt-40">
-          <span className="text-white">National</span>
-          <span className="text-purple-800 bg-black p-2 shadow-md  shadow-purple-900 skew-1">
-            Skateboarding
-          </span>
-          <span className="text-6xl font-zen text-[#39FF14]">Skate Off</span>
+        <div className="flex flex-col hero-title-line gap-4 text-center text-6xl  mt-40">
+          <div className="flex flex-col gap-4">
+            <span className="text-white font-zen">National</span>
+            <span className="text-purple-800 bg-black p-2 shadow-md font-zen  shadow-purple-900 skew-1">
+              Skateboarding
+            </span>
+            <span className="text-6xl font-zen text-[#39FF14]">Skate Off</span>
+          </div>
+          <p className="text-white font-mono text-2xl mt-10">
+            The biggest skateboarding competition in the world
+          </p>
+
+          <div className="flex gap-10 mx-auto">
+            <button className="text-black cursor-pointer text-xl font-bold bg-[#39FF14] w-40 p-2 rounded shadow-md shadow-black">
+              Get Tickets
+            </button>
+            <button className="cursor-pointer text-xl font-bold bg-white border-4 border-purple-600 text-purple-600 w-60 p-2 rounded shadow-md shadow-black">
+              Meet the Skaters
+            </button>
+          </div>
         </div>
       </div>
     </section>
