cf44e7133cd6fb3c90eab5bd4162bb2b00840094
Davonne <davonnejv@gmail.com>
2025-11-25 22:07:17 -0600

    feature: header section with styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,32 @@
 export default function Header() {
+  const ourNav = [
+    { name: "Register", href: "", id: 0 },
+    { name: "Skaters", href: "", id: 1 },
+    { name: "Events", href: "", id: 3 },
+    { name: "Sponsors", href: "", id: 4 },
+  ];
   return (
-    <header className="bg-black/90 shadow-lg w-full text-white p-4">
-      <h1 className="text-2xl font-bold">Go Ricter Skate Off</h1>
+    <header className="bg-black/90 shadow-lg w-full text-white p-4 flex items-center justify-between">
+      <img
+        src="./images/boarding.png"
+        alt="Black and white skateboard with grafitti, image by ChatGPT created by Davonne Vigil  "
+        className="w-20 shadow-md shadow-white"
+      />
+
+      <ul className="hidden md:flex gap-10">
+        {ourNav.map((item) => (
+          <li
+            key={item.id}
+            className="font-bold text-xl hover:bg-black/50 p-2 hover:scale-110 transition-all duration-300 ease-in-out  cursor-pointer  "
+          >
+            <a href={item.href}>{item.name}</a>
+          </li>
+        ))}
+      </ul>
+
+      <button className="font-mono text-xl cursor-pointer transition-all duration-300 ease-in-out hover:scale-110 shadow-xl shadow-black bg-white text-black  rounded p-2">
+        Get Tickets
+      </button>
     </header>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <section className="bg-[url('./images/skate.jpg')] bg-cover bg-center min-h-screen">
       <div>
-        <h1 className="text-white text-6x min-h-screenl">My Hero</h1>
+        <h1 className="text-white text-6x min-h-screen">My Hero</h1>
       </div>
     </section>
   );
